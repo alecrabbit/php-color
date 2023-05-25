7aef4d12c13cc5c4564f77ce11e28737f7da479a
Oleksii <oleksii.druzhchenko@gmail.com>
2023-05-25 15:14:15 +0300

    +files

--- src/Color/Color.php
+++ src/Color/Color.php
@@ -8,23 +8,6 @@ use AlecRabbit\Color\Contract\IColor;
 
 class Color implements IColor
 {
-    // int get alpha => (0xff000000 & value) >> 24;
-    //
-    //  /// The alpha channel of this color as a double.
-    //  ///
-    //  /// A value of 0.0 means this color is fully transparent. A value of 1.0 means
-    //  /// this color is fully opaque.
-    //  double get opacity => alpha / 0xFF;
-    //
-    //  /// The red channel of this color in an 8 bit value.
-    //  int get red => (0x00ff0000 & value) >> 16;
-    //
-    //  /// The green channel of this color in an 8 bit value.
-    //  int get green => (0x0000ff00 & value) >> 8;
-    //
-    //  /// The blue channel of this color in an 8 bit value.
-    //  int get blue => (0x000000ff & value) >> 0;
-
     private const MAX = 0xFFFFFFFF;
     private const SEGMENT = 0xFF;
     private int $value;
--- src/Color/Contract/IColor.php
+++ src/Color/Contract/IColor.php
@@ -13,4 +13,14 @@ interface IColor
     public static function fromARGB(int $alpha, int $r, int $g, int $b): IColor;
 
     public function getValue(): int;
+
+    public function getAlpha(): int;
+
+    public function getRed(): int;
+
+    public function getGreen(): int;
+
+    public function getBlue(): int;
+
+    public function getOpacity(): float;
 }
