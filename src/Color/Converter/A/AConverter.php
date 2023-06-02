886e3070dcc26ae1df51ab75dc91e95a6705f196
Oleksii <oleksii.druzhchenko@gmail.com>
2023-06-02 14:33:33 +0300

    ~

--- src/Color/Converter/A/AConverter.php
+++ src/Color/Converter/A/AConverter.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Color\Converter\A;
 
+use AlecRabbit\Color\Contract\IConvertableColor;
 use AlecRabbit\Color\Contract\IConverter;
 
 abstract class AConverter implements IConverter
 {
+    abstract public function convert(IConvertableColor $color): IConvertableColor;
 }
