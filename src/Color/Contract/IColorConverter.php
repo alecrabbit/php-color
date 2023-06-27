f16ebb04003d91a3e37e110b479eaf18aaabbc11
Oleksii <oleksii.druzhchenko@gmail.com>
2023-06-27 15:55:19 +0300

    ~

--- src/Color/Contract/IColorConverter.php
+++ src/Color/Contract/IColorConverter.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Color\Contract;
 
+use AlecRabbit\Color\Converter\ColorConverter;
+
 interface IColorConverter
 {
+    /**
+     * @param class-string $class
+     */
+    public function to(string $class): IConverter;
 }
