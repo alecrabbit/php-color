3096b378887b861df93e0b1a9b8b6a72855b3e0a
Oleksii <oleksii.druzhchenko@gmail.com>
2023-06-30 16:42:22 +0300

    ~

--- tests/Functional/Color/Instantiator/HexInstantiatorTest.php
+++ tests/Functional/Color/Instantiator/HexInstantiatorTest.php
@@ -7,14 +7,29 @@ namespace AlecRabbit\Tests\Functional\Color\Instantiator;
 use AlecRabbit\Color\Contract\IInstantiator;
 use AlecRabbit\Color\Instantiator\HexInstantiator;
 use AlecRabbit\Tests\TestCase\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 
 class HexInstantiatorTest extends TestCase
 {
-    #[Test]
-    public function instantiator(): void
+    public static function canInstantiateDataProvider(): iterable
     {
-        $this->assertTrue(true);
+        yield from [
+            [0xff0000,'#ff0000'],
+            [0x0,'#000'],
+            [0x0,'000'],
+            [0x008080,'teal'],
+            [0xffc0cb,'PINK'],
+        ];
+    }
+
+    #[Test]
+    #[DataProvider('canInstantiateDataProvider')]
+    public function canInstantiate(int $expected, string $incoming): void
+    {
+        $instantiator = $this->getTesteeInstance();
+
+        self::assertSame($expected, $instantiator->fromString($incoming)->getValue());
     }
 
     protected function getTesteeInstance(): IInstantiator
