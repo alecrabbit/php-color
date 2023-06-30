004c9a27712a1a30bb123f132e69f7d4941ab95b
Oleksii <oleksii.druzhchenko@gmail.com>
2023-06-30 16:56:53 +0300

    ~

--- tests/Functional/Color/Instantiator/HexInstantiatorTest.php
+++ tests/Functional/Color/Instantiator/HexInstantiatorTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Tests\Functional\Color\Instantiator;
 
+use AlecRabbit\Color\Contract\IHexColor;
 use AlecRabbit\Color\Contract\IInstantiator;
 use AlecRabbit\Color\Instantiator\HexInstantiator;
 use AlecRabbit\Tests\TestCase\TestCase;
@@ -15,11 +16,24 @@ class HexInstantiatorTest extends TestCase
     public static function canInstantiateDataProvider(): iterable
     {
         yield from [
+            // [(int)expected, (string)incoming]
+            [0xff0000, '#ff0000'],
+            [0xff0000, 'ff0000'],
+            [0xffff00, '#ff0'],
+            [0xffff00, 'ff0'],
+            [0xfff5ee, 'seashell'],
+            [0xfff5ee, 'fff5ee'],
+            [0xfff5ee, '#fff5ee'],
+            [0xfff5ee, 'SeaShell'],
+            [0x708090, 'slategray'],
+            [0x708090, 'slaTeGray'],
             [0xff0000,'#ff0000'],
             [0x0,'#000'],
             [0x0,'000'],
             [0x008080,'teal'],
             [0xffc0cb,'PINK'],
+            [0x48D1CC,'mediumTurquoise'],
+            [0x0f0e0d,'0f0e0d'],
         ];
     }
 
@@ -29,7 +43,9 @@ class HexInstantiatorTest extends TestCase
     {
         $instantiator = $this->getTesteeInstance();
 
-        self::assertSame($expected, $instantiator->fromString($incoming)->getValue());
+        $color = $instantiator->fromString($incoming);
+        self::assertInstanceOf(IHexColor::class, $color);
+        self::assertSame($expected, $color->getValue());
     }
 
     protected function getTesteeInstance(): IInstantiator
