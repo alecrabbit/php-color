5db701ee6f045e5d34fe61e7da12afb3d0722dd3
Oleksii <oleksii.druzhchenko@gmail.com>
2023-06-30 14:46:28 +0300

    ~readonly class

--- src/Color/Converter/DTO/DRGB.php
+++ src/Color/Converter/DTO/DRGB.php
@@ -7,12 +7,12 @@ namespace AlecRabbit\Color\Converter\DTO;
 /**
  * @codeCoverageIgnore Coverage does not make sense.
  */
-final class DRGB
+final readonly class DRGB
 {
     public function __construct(
-        public readonly int $red,
-        public readonly int $green,
-        public readonly int $blue,
+        public int $red,
+        public int $green,
+        public int $blue,
     ) {
     }
 }
