edf14377d06276628b99ea11b5b4a0745f1371ef
Oleksii <oleksii.druzhchenko@gmail.com>
2023-12-22 12:46:23 +0200

    ~refactor

--- src/Color/Contract/Gradient/IGradient.php
+++ src/Color/Contract/Gradient/IGradient.php
@@ -11,6 +11,10 @@ use Traversable;
 interface IGradient
 {
     /**
+     * Unwraps gradient to traversable of colors.
+     *
+     * @return Traversable<IColor>
+     *
      * @throws InvalidArgument
      */
     public function unwrap(): Traversable;
@@ -19,6 +23,7 @@ interface IGradient
      * Returns one color from gradient by index.
      *
      * @param int $index The index of the color. Starts from 0.
+     *
      * @return IColor
      *
      * @throws InvalidArgument
