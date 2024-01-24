82e047c2a3c976fd3fc7193cbc2dba7bd6c47c99
Oleksii <11045453+alecrabbit@users.noreply.github.com>
2024-01-24 12:09:03 +0200

    ~

--- src/Color/Contract/Gradient/Vector/IVector.php
+++ src/Color/Contract/Gradient/Vector/IVector.php
@@ -6,6 +6,9 @@ namespace AlecRabbit\Color\Contract\Gradient\Vector;
 
 interface IVector
 {
+    /**
+     * // TODO (2024-01-24 12:07) [Alec Rabbit]: it seems like this method does not belong here
+     */
     public static function create(int|float $start, int|float $end, int $count = null, int $precision = null): IVector;
 
     public function get(int|float $y = null): float;
--- src/Color/Gradient/Vector/Vector.php
+++ src/Color/Gradient/Vector/Vector.php
@@ -10,7 +10,7 @@ use function abs;
 
 final readonly class Vector implements IVector
 {
-    // TODO (2024-01-23 15:57) [Alec Rabbit]: add namespaced const CALCULATION_PRECISION = 6; [c16baba0-8d27-42f4-bc27-bf9d771c16f7]
+    protected const CALCULATION_PRECISION = 6;
 
     public function __construct(
         public int|float $x,
@@ -28,7 +28,7 @@ final readonly class Vector implements IVector
         return new self(
             $start,
             self::calculateStep($start, $end, $count),
-            $precision ?? 6, // todo [c16baba0-8d27-42f4-bc27-bf9d771c16f7]
+            $precision ?? self::CALCULATION_PRECISION,
         );
     }
 
